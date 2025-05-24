98bb1213fcda6ffe94703344f3ce54155d0bf10b
Rotsy Rakotondrainy <rakotondrainyrotsy@gmail.com>
2025-05-24 13:04:52 +0300

    Update index.js

--- back/index.js
+++ back/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Servir les fichiers statiques dans "public"
 app.use(express.static('public'));
